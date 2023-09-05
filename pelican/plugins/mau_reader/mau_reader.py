79a1a22eb6840a49fd761f42ed9eab34aa460501
Leonardo Giordani <giordani.leonardo@gmail.com>
2023-09-05 15:14:08 +0100

    Changed code to use Mau3

--- pelican/plugins/mau_reader/mau_reader.py
+++ pelican/plugins/mau_reader/mau_reader.py
@@ -1,6 +1,7 @@
 """Mau Reader plugin.
 
 This plugin allows you to use Mau to write your posts. File extension should be `.mau`.
+
 """
 
 from pelican import signals
@@ -8,11 +9,26 @@ from pelican.readers import BaseReader
 from pelican.utils import pelican_open
 
 try:
-    from mau import Mau
+    from mau import Mau, load_visitors
 
+    visitor_classes = load_visitors()
     mau_enabled = True
 except ImportError:
+    visitor_classes = []
     mau_enabled = False
+
+
+visitors = {i.format_code: i for i in visitor_classes}
+
+
+class OutputFormatNotSupported(Exception):
+    """Exception to signal output format not supported."""
+
+    def __init__(self, output_format):
+        super().__init__(
+            f"Output format {output_format} is not supported by Mau. "
+            "You might have to install a plugin."
+        )
 
 
 class MauReader(BaseReader):
@@ -28,14 +44,33 @@ class MauReader(BaseReader):
         config = self.settings.get("MAU", {})
 
         output_format = config.get("output_format", "html")
+
+        if output_format not in visitors:
+            raise OutputFormatNotSupported(output_format)
+
         custom_templates = config.get("custom_templates", {})
+        templates_directory = config.get("templates_directory", None)
+        visitor_class = visitors[output_format]
 
         self._source_path = source_path
-        self._mau = Mau(config, output_format, custom_templates=custom_templates)
-        with pelican_open(source_path) as text:
-            content = self._mau.process(text)
+        self._mau = Mau(
+            source_path,
+            visitor_class=visitor_class,
+            config=config,
+            custom_templates=custom_templates,
+            templates_directory=templates_directory,
+        )
 
-        metadata = self._parse_metadata(self._mau.variables["pelican"])
+        with pelican_open(source_path) as text:
+            lexer = self._mau.run_lexer(text)
+
+        parser = self._mau.run_parser(lexer.tokens)
+        content = self._mau.process(parser.nodes, parser.environment)
+
+        if visitor_class.transform:
+            content = visitor_class.transform(content)
+
+        metadata = self._parse_metadata(self._mau.environment.asdict()["pelican"])
 
         return content, metadata
 
@@ -47,7 +82,9 @@ class MauReader(BaseReader):
         for name, value in meta.items():
             name = name.lower()
             if name in formatted_fields:
-                formatted = self._mau.process(value)
+                lexer = self._mau.run_lexer(value)
+                parser = self._mau.run_parser(lexer.tokens)
+                formatted = self._mau.process(parser.nodes, parser.environment)
                 output[name] = self.process_metadata(name, formatted)
             elif len(value) > 1:
                 # handle list metadata as list of string
